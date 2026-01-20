57a8241775c14e5f52288633d57bad57e4947483
Cristian Lluis <cristian@lluis.ch>
2026-01-20 14:45:16 +0100

    fix env source script

--- functions/source_env.sh
+++ functions/source_env.sh
@@ -1,6 +1,6 @@
 # Searches for a virtual env in /. and in /venv. Then activates it.
 sc() {
-    if source venv/bin/activate 2>/dev/null || source bin/activate 2>/dev/null; then
+    if source venv/bin/activate 2>/dev/null || source bin/activate 2>/dev/null|| source .venv/bin/activate 2>/dev/null; then
         # Virtual environment activated successfully
         return 0
     else
